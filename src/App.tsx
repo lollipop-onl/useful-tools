8be752099373619946c6dba28b143a3a85a8295a
lollipop-onl <lollipop@simochee.net>
2023-07-20 17:21:53 +0900

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export const App: React.FC = () => {
+  return (
+    <div className='max-w-screen-md mx-auto px-4 py-10'>
+      <article className='prose'>
+        <h1>react-app</h1>
+        <p>This is a React application template.</p>
+        <p>with Vite, TypeScript, TailwindCSS and daisyUI</p>
+      </article>
+    </div>
+  );
+};
